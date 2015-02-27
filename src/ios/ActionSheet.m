1024af59fca7d3191707eb4c63dfd9394dac0961
EddyVerbruggen <eddyverbruggen@gmail.com>
2015-02-27 21:51:20 +0100

    #10 use a background thread

--- src/ios/ActionSheet.m
+++ src/ios/ActionSheet.m
@@ -13,22 +13,27 @@ NSString* theCallbackId;
     NSString *addCancelButtonWithLabel = [options objectForKey:@"addCancelButtonWithLabel"] ?: nil;
     NSString *addDestructiveButtonWithLabel = [options objectForKey:@"addDestructiveButtonWithLabel"] ?: nil;
 
-    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:title
-                                                             delegate:self
-                                                    cancelButtonTitle:nil
-                                               destructiveButtonTitle:addDestructiveButtonWithLabel
-                                                    otherButtonTitles:nil];
+    [self.commandDelegate runInBackground:^{
 
-    for(int i = 0; i < [buttons count]; i++) {
-        [actionSheet addButtonWithTitle:[buttons objectAtIndex:i]];
-    }
-
-    if (addCancelButtonWithLabel != nil) {
-        [actionSheet addButtonWithTitle:addCancelButtonWithLabel];
-        actionSheet.cancelButtonIndex = [buttons count]+(addDestructiveButtonWithLabel == nil ? 0 : 1);
-    }
-
-    [actionSheet showInView:self.webView.superview];
+        UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:title
+                                                                 delegate:self
+                                                        cancelButtonTitle:nil
+                                                   destructiveButtonTitle:addDestructiveButtonWithLabel
+                                                        otherButtonTitles:nil];
+  
+        for(int i = 0; i < [buttons count]; i++) {
+            [actionSheet addButtonWithTitle:[buttons objectAtIndex:i]];
+        }
+  
+        if (addCancelButtonWithLabel != nil) {
+            [actionSheet addButtonWithTitle:addCancelButtonWithLabel];
+            actionSheet.cancelButtonIndex = [buttons count]+(addDestructiveButtonWithLabel == nil ? 0 : 1);
+        }
+  
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [actionSheet showInView:self.webView.superview];
+        });
+    }];
 }
 
 // delegate function of UIActionSheetDelegate
